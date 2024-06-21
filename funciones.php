d35d8ce26614e066f5bdd0fb45ec553cbad107f1
unicentropasto <71729827+unicentropasto@users.noreply.github.com>
2024-06-21 11:24:37 -0500

    Se cambian credenciales

--- funciones.php
+++ funciones.php
@@ -51,7 +51,7 @@ function conectarbd()
   //conexion local
   //$conexion = pg_connect("host='127.0.0.1' port='5432' dbname='sisbolbd_uc' user='postgres' password='12996873'") or die('No se ha podido conectar: ' . pg_last_error());
   //conexion cloud
-  $conexion=pg_connect("host='ec2-52-5-110-35.compute-1.amazonaws.com' port='5432' dbname='d1nlhajk5a952t' user='mdbsvljjjfhiri' password='6636d895f0ec9fc782b771f75f6a72ca1568adf7c74efe5f8bdc96b2673db617'") or die('No se ha podido conectar: ' . pg_last_error());
+  $conexion=pg_connect("host='ccqnant9i80rgh.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com' port='5432' dbname='d7kmu10ihe35b8' user='u7vrtllppvssi9' password='p9946f49472d3d7bf499aa8f17266c4f12309aa1451a1120dd6c6a322c1d0e2c2'") or die('No se ha podido conectar: ' . pg_last_error());
   if(!$conexion){
     echo "No hay conexión con la BD";
   }
